e874e89bbc67e79cdeea89175141bb97cbe5ca32
Peni <joo67@hotmail.com>
2022-11-17 16:24:59 +0200

    Added some testing to slice/words

--- test/eq.test.js
+++ test/eq.test.js
@@ -1,1 +1,4 @@
 // Joona
+import chai from 'chai'
+import isEmpty from "../COMP.SE.200-2022-2023-1-main/src/isEmpty.js"
+const expect = chai.expect
--- test/slice.test.js
+++ test/slice.test.js
@@ -1,1 +1,25 @@
 // Joona
+import chai from 'chai'
+import slice from "../COMP.SE.200-2022-2023-1-main/src/slice.js"
+const expect = chai.expect
+
+describe("slice", () => {
+    let array = [];
+    it("Slice array starting from third element to the last", () =>{
+        array = [1, 2, 3, 4];
+        var arr = slice(array, 2);
+        expect(arr).to.eql([3,4]);
+    });
+
+    it("Slice array with start at -3", () =>{
+        array = [1, 2, 3, 4];
+        var test = slice(array, -3);
+        expect(test).to.eql([2, 3,4]);
+    });
+
+    it("Slice array from 1 to 2", () =>{
+        array = [1, 2, 3, 4];
+        var test = slice(array, 1, 2);
+        expect(test).to.eql([2]);
+    });
+})
