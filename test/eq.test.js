1b1c20364d4d99a490578116d87e85afd1f3cf81
Peni <joo67@hotmail.com>
2022-11-17 17:24:31 +0200

    Add tests to eq and filter

--- test/eq.test.js
+++ test/eq.test.js
@@ -1,4 +1,27 @@
 // Joona
 import chai from 'chai'
-import isEmpty from "../COMP.SE.200-2022-2023-1-main/src/isEmpty.js"
+import eq from "../COMP.SE.200-2022-2023-1-main/src/eq.js"
 const expect = chai.expect
+
+describe("eq", () => {
+    it("Compare identical numbers", () =>{
+        expect(eq(3, 3)).to.equal(true);
+    });
+
+    it("Compare different numbers", () =>{
+        expect(eq(3, 56)).to.equal(false);
+    });
+
+    it("Compare identical strings", () =>{
+        expect(eq("mikko", "mikko")).to.equal(true);
+    });
+
+    it("Compare different string", () =>{
+        expect(eq("mikko", "maria")).to.equal(false);
+    });
+
+    it("Compare undefined to true", () =>{
+        expect(eq(undefined, true)).to.equal(false);
+    });
+
+});
