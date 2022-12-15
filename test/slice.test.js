ede141340aba208c3dc2ed7bea06d91482b53cd2
Peni <joo67@hotmail.com>
2022-12-15 21:02:41 +0200

    Additional testing to slice.js

--- test/slice.test.js
+++ test/slice.test.js
@@ -7,8 +7,8 @@ describe("slice", () => {
     let array = [];
     it("Slice array starting from third element to the last", () =>{
         array = [1, 2, 3, 4];
-        var arr = slice(array, 2);
-        expect(arr).to.eql([3,4]);
+        var test = slice(array, 2);
+        expect(test).to.eql([3,4]);
     });
 
     it("Slice array with start at -3", () =>{
@@ -21,5 +21,35 @@ describe("slice", () => {
         array = [1, 2, 3, 4];
         var test = slice(array, 1, 2);
         expect(test).to.eql([2]);
+    });
+
+    it("Slice empty array", () =>{
+        array = [];
+        var test = slice(array, 1, 2);
+        expect(test).to.eql([]);
+    });
+
+    it("Slice with end later than start", () =>{
+        array = [];
+        var test = slice(array, 2, 1);
+        expect(test).to.eql([]);
+    });
+
+    it("Slice with -3 to -4", () =>{
+        array = [1, 2, 3, 4];
+        var test = slice(array, -3, -4);
+        expect(test).to.eql([]);
+    });
+
+    it("Slice with -4 to -3", () =>{
+        array = [1, 2, 3, 4];
+        var test = slice(array, -4, -3);
+        expect(test).to.eql([1]);
+    });
+
+    it("Slice a whole sparse array to return dense", () =>{
+        array = [1, , 3, 4];
+        var test = slice(array, 0, 4);
+        expect(test).to.eql([1, 3, 4]);
     });
 })
